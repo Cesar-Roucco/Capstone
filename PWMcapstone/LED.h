2508514d9bc21ae1099ef2c88825df5f9c4468c9
Ayman Mobin <aymamob@gmail.com>
2018-12-06 10:36:48 -0500

    led pwm

---
+++ PWMcapstone/LED.h
@@ -0,0 +1,23 @@
+#ifndef LED_H
+#define LED_H
+
+#include <msp430.h>
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *                 LED
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *   GPIO      :  P1.4
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+#define LED_BIT					BIT4
+#define LED_PORT				P1OUT
+#define LED_DDR					P1DIR
+#define SET_LED_AS_AN_OUTPUT	LED_DDR |= LED_BIT
+#define TURN_ON_LED				LED_PORT |= LED_BIT
+#define TURN_OFF_LED			LED_PORT &= ~LED_BIT
+#define TOGGLE_LED				LED_PORT ^= LED_BIT
+
+// Prototypes
+void InitializeLEDPortPin(void);
+
+#endif
